d169f9e6ccea652b8873066c6f3982468435b0c5
Ceda <pleskac@gmail.com>
2014-03-28 13:34:18 +0100

    My first init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eletronictesting::Application.config.secret_key_base = 'b2c8faa1a67ed364a425ccba00ef5fbeba57b73fcd9b090a699368eb88ee6e9cfb5cbe0e8ad035f403fa0d51039e6d440753f479ef4996ed6d9a6cd151bde82f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eletronictesting::Application.config.session_store :cookie_store, key: '_eletronictesting_session'
